c7978507cd3ade89f95d72a667168d720c9d0609
Phillip Elliott <phillipcharleselliott@gmail.com>
2018-04-18 23:57:21 +0000

    modified csv path, added my map quest key

--- geojson.php
+++ geojson.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIMD6F8kpk98E0Np1DMfXjb3qD_4mfMz-pLycXUi_NtiiMybR2GyjjelfnGLUnwjvapsNe2blyNgtN/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
